55f3da5e0f4e4835528305b1c7b2fe963a06cccd
igorraven <igorraven@yahoo.com>
2017-04-28 22:28:44 -0700

    first commit

---
+++ awesomeProject/TestGo1.go
@@ -0,0 +1,20 @@
+//  Kindle An Introduction To Programming GO  
+//  12%  (192 of 1753)  Nook page #7
+
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func main() {
+	fmt.Println("")
+	fmt.Println("\n\t Hello World2")
+}
+
+
+
+
+
---
+++ chapter2/HelloWorldJB.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+	num1 = 3
+	num2 = 9
+
+	fmt.Println("\n\t Hello World From Jet Brain!")
+	fmt.Printf("\n\t ( Num1= %d  Num2= %d ) = %d", num1, num2, num1+num2)
+}
